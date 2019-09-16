6338c477d37d071893329907205d700d9a44bdc9
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-16 14:08:27 -0700

    added EntryDetailController details

---
+++ Journal/ViewControllers/EntryDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EntryDetailViewController.swift
+//  Journal
+//
+//  Created by Taylor Lyles on 9/16/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+	
+	@IBOutlet weak var textField: UITextField!
+	@IBOutlet weak var textView: UITextView!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	@IBAction func save(_ sender: Any) {
+	}
+	
+}
